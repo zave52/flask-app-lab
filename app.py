5ab8c9b2ec6a03a57b634aa8d6542532fa9150b9
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-10-10 19:48:10 +0300

    Added resume and contacts route in app.py with inject_current_year context function

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, Response
+from datetime import datetime
 
 app = Flask(__name__)
 
 
+@app.context_processor
+def inject_current_year() -> dict[str, int]:
+    return {'current_year': datetime.now().year}
+
+
 @app.route("/")
-def main() -> str:
-    return "Hello world!"
+def main() -> Response:
+    return redirect(url_for("resume"))
+
+
+@app.route("/resume")
+def resume() -> str:
+    return render_template("resume.html")
+
+
+@app.route("/contacts")
+def contacts() -> str:
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
